da7be8150016fad8926b12896412ca2be02630e2
Samsher135 <12345samsingh.samu@gmail.com>
2021-03-25 19:07:49 +0530

    Login

--- src/app.js
+++ src/app.js
@@ -3,18 +3,25 @@
 const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8000 ;
+const hbs = require('hbs');
 // var MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 
 const staticPath = path.join(__dirname, "../public");
+const partialsPath = path.join(__dirname, "../views/partials")
 
-app.set("view engine", "hbs")
+app.set("view engine", "hbs");
+hbs.registerPartials(partialsPath);
 
 app.use(express.static(staticPath));
 
 app.get("/", (req, res) =>{
     res.render("index");
+});
+
+app.get("/Login", (req, res) =>{
+    res.render("Login");
 });
 
 app.listen(port, () => {
