21cf56993b0f011cca3c00e8cad1ee50caed1019
Samsher135 <12345samsingh.samu@gmail.com>
2021-03-27 21:14:41 +0530

    flex

---
+++ Downloads/Bitleaf-master_mihir/src/app.js
@@ -0,0 +1,33 @@
+// var AWS = require("aws-sdk");
+// const mongoose = require('mongoose');
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8000 ;
+const hbs = require('hbs');
+// var MongoClient = require('mongodb').MongoClient;
+
+const app = express();
+
+const staticPath = path.join(__dirname, "../public");
+const partialsPath = path.join(__dirname, "../views/partials")
+
+app.set("view engine", "hbs");
+hbs.registerPartials(partialsPath);
+
+app.use(express.static(staticPath));
+
+app.get("/", (req, res) =>{
+    res.render("index");
+});
+
+app.get("/Dashboard", (req, res) =>{
+    res.render("Dashboard");
+});
+
+app.get("/warnings", (req, res) =>{
+    res.render("warnings");
+});
+
+app.listen(port, () => {
+    console.log(`listening to ${port}`);
+  })
