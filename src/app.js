09e45255c6b9ebe0b8907aa10f7a7f4442860680
Samsher135 <12345samsingh.samu@gmail.com>
2021-04-09 15:06:44 +0530

    update gauge

--- src/app.js
+++ src/app.js
@@ -4,7 +4,22 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8000 ;
 const hbs = require('hbs');
-// var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://All_users_25:test@cluster0.mjn0g.mongodb.net/test?retryWrites=true&w=majority";
+
+function database() {
+    MongoClient.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true }, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("test");
+        dbo.collection("parameters").find({}).toArray(function(err, result) {
+          if (err) throw err;
+          console.log(result);
+          value = result[0].value;
+          value1 = result[1].value; 
+          db.close();
+        });
+      });
+  }
 
 const app = express();
 
@@ -21,7 +36,11 @@ app.get("/", (req, res) =>{
 });
 
 app.get("/Dashboard", (req, res) =>{
-    res.render("Dashboard");
+    database();
+    res.render("Dashboard", {
+        p_value : value,
+        p_value1 : value1,
+    });
 });
 
 app.get("/warnings", (req, res) =>{
